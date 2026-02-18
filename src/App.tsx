77ccb320fa026dc4bb60ca187511fdc8c4084450
HenriqueBeserrA <beserrahnrq@gmail.com>
2026-02-18 09:19:59 -0300

    chore:  criação da rota privada

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,23 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Login from './pages/login/Login';
 import HomePage from './pages/home/Home';
+import PrivateRoute from './PrivateRoutes/PrivateRoutes';
+
 
 function App() {
+	
+	
+	
 	return (
 		<Routes>
 			<Route path={'/login'} element={<Login />} />
-			<Route path='/home' element={<HomePage />} />
+			<Route element={ <PrivateRoute/> }>
+				<Route path={'/home'} element={<HomePage />} />	
+			</Route>
 		</Routes>
+
 	);
 }
+
 
 export default App;
